e4db5452c8831a41fc32135630666c82d4abbce8
vogdb <vozhdb@gmail.com>
2013-06-06 13:57:04 +0400

    The npm's coding style.
    Add CONTRIBUTING.md. Format code by the npm's coding style.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,65 @@
 /*global module:false*/
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg:grunt.file.readJSON('package.json'),
-        banner:'/*!\n <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            ' <%= (pkg.homepage ? "* " + pkg.homepage : pkg.repository.url) + "\\n" %>' +
-            ' Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
-            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n*/\n',
-        jshint:{
-            options:{
-                curly:true,
-                eqeqeq:true,
-                immed:true,
-                latedef:true,
-                newcap:true,
-                noarg:true,
-                sub:true,
-                undef:true,
-                unused:true,
-                boss:true,
-                eqnull:true,
-                browser:true,
-                asi:true,
-                globals:{}
-            },
-            gruntfile:{
-                src:'Gruntfile.js'
-            },
-            lib_test:{
-                src:['lib/**/*.js', 'test/**/*.js']
-            }
-        },
-        uglify:{
-            options:{
-                banner:'<%= banner %>'
-            },
-            dist:{
-                src:'src/SelectLayers.js',
-                dest:'dist/leaflet.select-layers.min.js'
-            }
-        },
-        watch:{
-            gruntfile:{
-                files:'<%= jshint.gruntfile.src %>',
-                tasks:['jshint:gruntfile']
-            }
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json')
+
+    , banner: '/*!\n <%= pkg.title || pkg.name %> - v<%= pkg.version %> - '
+      + '<%= grunt.template.today("yyyy-mm-dd") %>\n'
+      + ' <%= (pkg.homepage ? "* " + pkg.homepage : pkg.repository.url) + "\\n" %>'
+      + ' Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;'
+      + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n*/\n'
+
+    , jshint: {
+        options: {
+          curly: true
+          , eqeqeq: true
+          , immed: true
+          , latedef: true
+          , newcap: true
+          , noarg: true
+          , sub: true
+          , undef: true
+          , unused: true
+          , boss: true
+          , eqnull: true
+          , browser: true
+          , asi: true
+          , globals: {}
         }
-    })
+      , gruntfile: {
+          src: 'Gruntfile.js'
+      }
+      , lib_test: {
+          src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    }
 
-    grunt.loadNpmTasks('grunt-contrib-jshint')
-    grunt.loadNpmTasks('grunt-contrib-watch')
-    grunt.loadNpmTasks('grunt-contrib-uglify')
+    , uglify: {
+        options: {
+          banner: '<%= banner %>'
+        }
+        , dist: {
+            src: 'src/SelectLayers.js'
+            , dest: 'dist/leaflet.select-layers.min.js'
+      }
+    }
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'uglify'])
+    , watch: {
+        gruntfile: {
+          files: '<%= jshint.gruntfile.src %>'
+          , tasks: ['jshint:gruntfile']
+        }
+    }
+
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'uglify'])
 
 }
--- src/SelectLayers.js
+++ src/SelectLayers.js
@@ -4,93 +4,93 @@
 
 L.Control.SelectLayers = L.Control.ActiveLayers.extend({
 
-    _initLayout: function () {
-        var className = 'leaflet-control-layers',
-            container = this._container = L.DomUtil.create('div', className);
+  _initLayout: function () {
+    var className = 'leaflet-control-layers'
+    var container = this._container = L.DomUtil.create('div', className)
 
-        L.DomEvent.disableClickPropagation(container);
-        if (!L.Browser.touch) {
-            L.DomEvent.on(container, 'mousewheel', L.DomEvent.stopPropagation);
-        } else {
-            L.DomEvent.on(container, 'click', L.DomEvent.stopPropagation);
-        }
-
-        var form = this._form = L.DomUtil.create('form', className + '-list');
-
-        if (this.options.collapsed) {
-            var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container);
-            link.href = '#';
-            link.title = 'Layers';
-
-            if (L.Browser.touch) {
-                L.DomEvent
-                    .on(link, 'click', L.DomEvent.stopPropagation)
-                    .on(link, 'click', L.DomEvent.preventDefault)
-                    .on(link, 'click', this._expand, this);
-            } else {
-                L.DomEvent
-                    .on(container, 'mouseover', this._expand, this)
-                    .on(container, 'mouseout', this._collapse, this);
-                L.DomEvent.on(link, 'focus', this._expand, this);
-            }
-
-            this._map.on('movestart', this._collapse, this);
-        } else {
-            this._expand();
-        }
-
-        this._baseLayersList = L.DomUtil.create('select', className + '-base', form);
-        L.DomEvent.on(this._baseLayersList, 'change', this._onBaseLayerOptionChange, this)
-        this._separator = L.DomUtil.create('div', className + '-separator', form);
-        this._overlaysList = L.DomUtil.create('div', className + '-overlays', form);
-
-        container.appendChild(form);
-    },
-
-    _onBaseLayerOptionChange: function () {
-        var selectedLayerIndex = this._baseLayersList.selectedIndex
-        var selectedLayerOption = this._baseLayersList.options[selectedLayerIndex]
-        var selectedLayer = this._layers[selectedLayerOption.layerId]
-
-        this._changeBaseLayer(selectedLayer.layer)
-    },
-
-    _changeBaseLayer: function (layer) {
-        //to be compatible with parent
-        this._handlingClick = true;
-
-        this._map.addLayer(layer)
-        this._map.removeLayer(this._activeBaseLayer)
-        this._map.setZoom(this._map.getZoom());
-        this._map.fire('baselayerchange', {layer: layer});
-        this._activeBaseLayer = layer
-
-        //to be compatible with parent
-        this._handlingClick = false;
-    },
-
-    _addItem: function (obj) {
-        if (obj.overlay) {
-            //overlay items is handled the same as before
-            return L.Control.Layers.prototype._addItem.call(this, obj)
-        } else {
-            var option = this._createOptionElement(obj)
-            this._baseLayersList.appendChild(option)
-        }
-    },
-
-    _createOptionElement: function (obj) {
-        var option = document.createElement('option')
-        option.layerId = L.stamp(obj.layer)
-        option.innerHTML = obj.name
-        if (this._map.hasLayer(obj.layer)) {
-            option.setAttribute('selected', true)
-        }
-        return option
+    L.DomEvent.disableClickPropagation(container)
+    if (!L.Browser.touch) {
+      L.DomEvent.on(container, 'mousewheel', L.DomEvent.stopPropagation)
+    } else {
+      L.DomEvent.on(container, 'click', L.DomEvent.stopPropagation)
     }
+
+    var form = this._form = L.DomUtil.create('form', className + '-list')
+
+    if (this.options.collapsed) {
+      var link = this._layersLink = L.DomUtil.create('a', className + '-toggle', container)
+      link.href = '#'
+      link.title = 'Layers'
+
+      if (L.Browser.touch) {
+        L.DomEvent
+          .on(link, 'click', L.DomEvent.stopPropagation)
+          .on(link, 'click', L.DomEvent.preventDefault)
+          .on(link, 'click', this._expand, this)
+      } else {
+        L.DomEvent
+          .on(container, 'mouseover', this._expand, this)
+          .on(container, 'mouseout', this._collapse, this)
+        L.DomEvent.on(link, 'focus', this._expand, this)
+      }
+
+      this._map.on('movestart', this._collapse, this)
+    } else {
+      this._expand()
+    }
+
+    this._baseLayersList = L.DomUtil.create('select', className + '-base', form)
+    L.DomEvent.on(this._baseLayersList, 'change', this._onBaseLayerOptionChange, this)
+    this._separator = L.DomUtil.create('div', className + '-separator', form)
+    this._overlaysList = L.DomUtil.create('div', className + '-overlays', form)
+
+    container.appendChild(form)
+  },
+
+  _onBaseLayerOptionChange: function () {
+    var selectedLayerIndex = this._baseLayersList.selectedIndex
+    var selectedLayerOption = this._baseLayersList.options[selectedLayerIndex]
+    var selectedLayer = this._layers[selectedLayerOption.layerId]
+
+    this._changeBaseLayer(selectedLayer.layer)
+  },
+
+  _changeBaseLayer: function (layer) {
+    //to be compatible with parent
+    this._handlingClick = true
+
+    this._map.addLayer(layer)
+    this._map.removeLayer(this._activeBaseLayer)
+    this._map.setZoom(this._map.getZoom())
+    this._map.fire('baselayerchange', {layer: layer})
+    this._activeBaseLayer = layer
+
+    //to be compatible with parent
+    this._handlingClick = false
+  },
+
+  _addItem: function (obj) {
+    if (obj.overlay) {
+      //overlay items is handled the same as before
+      return L.Control.Layers.prototype._addItem.call(this, obj)
+    } else {
+      var option = this._createOptionElement(obj)
+      this._baseLayersList.appendChild(option)
+    }
+  },
+
+  _createOptionElement: function (obj) {
+    var option = document.createElement('option')
+    option.layerId = L.stamp(obj.layer)
+    option.innerHTML = obj.name
+    if (this._map.hasLayer(obj.layer)) {
+      option.setAttribute('selected', true)
+    }
+    return option
+  }
 
 })
 
 L.control.selectLayers = function (baseLayers, overlays, options) {
-    return new L.Control.SelectLayers(baseLayers, overlays, options);
-};
+  return new L.Control.SelectLayers(baseLayers, overlays, options)
+}
